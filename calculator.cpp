e4386c64136a0cbb968e69dd1af904f91d894190
YaroslavKopailo <yaroslavkopailo92@gmail.com>
2021-12-23 10:26:59 +0300

    Lab1

---
+++ calculator.cpp
@@ -0,0 +1,27 @@
+#include "calculator.h"
+
+int Calculator::Add (double a, double b)
+{
+    return a + b + 0.5;
+}
+
+int Calculator::Sub (double a, double b)
+{
+    return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+
+// Was
+// Wollen
+// Wir
+// TRINKEN
+// SIEBEN
+// TAGE
+// LANG
+// WAS
+// WOLLEN WIR TRINKEN 
+// SO EIN DURST
